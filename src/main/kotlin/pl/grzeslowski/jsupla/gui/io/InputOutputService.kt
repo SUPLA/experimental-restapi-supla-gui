78aeb5265907deb66361ee08cdc343718fa39355
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-08-15 15:57:18 +0200

    #1 Add licence header

--- src/main/kotlin/pl/grzeslowski/jsupla/gui/io/InputOutputService.kt
+++ src/main/kotlin/pl/grzeslowski/jsupla/gui/io/InputOutputService.kt
@@ -1,3 +1,16 @@
+// Copyright (C) AC SOFTWARE SP. Z O.O.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 package pl.grzeslowski.jsupla.gui.io
 
 import java.io.ByteArrayOutputStream
@@ -9,5 +22,5 @@ interface InputOutputService {
 
     fun readFile(filename: String): String?
 
-    fun writeToFile(filename: String, text: ByteArrayOutputStream);
+    fun writeToFile(filename: String, text: ByteArrayOutputStream)
 }
